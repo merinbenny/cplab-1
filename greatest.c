58de5a5e0cf681c81030df9440e6dcdcf59651c1
jiyamathai <44498560+jiyamathai@users.noreply.github.com>
2019-02-13 12:20:04 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,20 @@
+/*Program to find greatest of two numbers
+Author:Jiya Mathai
+Date:13/02/2019
+*/
+
+#include<stdio.h>
+void main()
+{
+	int number1,number2;
+	printf("enter two integer numbers:");
+	scanf("%d%d",&number1,&number2);
+	/*to find which number is greatest*/
+	if(number1>number2)
+		printf("%d greater",number1);
+	else
+		printf("%d greater",number2);
+}
+
+
+
